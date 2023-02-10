b29f0b3e62b396905579dcd51954d54ee38ed16f
asadraiyan <asadraiyan001@gmail.com>
2023-02-10 11:42:59 +0530

    Modified about page

--- src/Components/Heroimg3.js
+++ src/Components/Heroimg3.js
@@ -4,8 +4,12 @@ const Heroimg3 = () => {
   return (
     <div className='hero3-img'>
             <div className="about-content">
-                <h1>About Me</h1>
-                <p>I'm a friendly Front-End Developer.</p>
+                <h1>A little bit about myself !</h1>
+                <p>I'm a mechanical engineer, worked in my field for about 2.5 years and then i started learning coding in my free time but later on day by day my passion towards coding is increasing and then out of curiosity i joined web development course where i upgraded my skils and made some projects, Now i wanted to pursue my career as a frontend developer.</p>
+                <p>Well-organised person, problem solver and have a serious passion for building UI,
+                  Interested in the entire frontend spectrum and working on ambitious projects with positive people.
+
+</p>
             </div>
 
 
