d686de4574cec52bad95a7860196ea8b980aee0f
asadraiyan <asadraiyan001@gmail.com>
2023-02-11 16:18:55 +0530

    Made contact page responsive

--- src/Components/Contact/components/ContactImage.js
+++ src/Components/Contact/components/ContactImage.js
@@ -3,8 +3,8 @@ const ContactImage = () => {
   return (
     <div className='hero4-img'>
       <div className="contact-content">
-        <h1>Contact Me</h1>
-        <p>Let's have a chat</p>
+        <h1 className='contact-me'>Contact Me</h1>
+        <p className='chat'>Let's have a chat</p>
       </div>
 
 
