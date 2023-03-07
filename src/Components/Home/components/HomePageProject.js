7fce81833af4fb3251e4a9a8f2fcdc6242b61508
asadraiyan <asadraiyan001@gmail.com>
2023-03-07 13:24:12 +0530

    added 2 more projects in project section

--- src/Components/Home/components/HomePageProject.js
+++ src/Components/Home/components/HomePageProject.js
@@ -11,9 +11,24 @@ const HomePageProject = () => {
         </div>
         <div className="project-container">
           <div className="card-1">
+            <h2 className='title'>Weather App</h2>
+            <small className='date'>
+              <time>6th March, 2023</time>
+            </small>
+            <p className='content-1'>Created an app using React.Js. A real time weather app which shows current weather of a particular city in the world.</p>
+            <div className="btn-container-1">
+              <button className='btn-3'>
+                <a href="https://react-weatherr-appp.netlify.app/" className='anchor-1' target="_blank">View Live</a>
+              </button>
+              <button className='btn-4'>
+                <a href="https://github.com/asadraiyan/react-weather-app" className='anchor-1' target="_blank">View Source</a>
+              </button>
+            </div>
+          </div>
+          <div className="card-1">
             <h2 className='title'>Food Gallery App</h2>
             <small className='date'>
-              <time>27th January, 2023</time>
+              <time>10th February, 2023</time>
             </small>
             <p className='content-1'>Created an app using React.Js. It is a food gallery app which allows you to select different types of food categories.</p>
             <div className="btn-container-1">
@@ -28,7 +43,7 @@ const HomePageProject = () => {
           <div className="card-1">
             <h2 className='title'>Quiz App</h2>
             <small className='date'>
-              <time>31st January, 2023</time>
+              <time>5th February, 2023</time>
             </small>
             <p className='content-1'>Created an app using React.Js. It is an application which asks different types of questions based on web development.</p>
             <div className="btn-container-1">
@@ -40,21 +55,7 @@ const HomePageProject = () => {
               </button>
             </div>
           </div>
-          <div className="card-1">
-            <h2 className='title'>Calendar App</h2>
-            <small className='date'>
-              <time>14th January, 2023</time>
-            </small>
-            <p className='content-1'>Created an app using HTML CSS & Javascript. A JavaScript Calendar which display the date and days of the week.</p>
-            <div className="btn-container-1">
-              <button className='btn-3'>
-                <a href="https://dynamic-calendar-app.netlify.app/" className='anchor-1' target="_blank">View Live</a>
-              </button>
-              <button className='btn-4'>
-                <a href="https://github.com/asadraiyan/Calendar-app" className='anchor-1' target="_blank">View Source</a>
-              </button>
-            </div>
-          </div>
+
         </div>
         <div className="buttons">
           <button className='view-all'> <Link className='anchor-1' to="/Projects" > View All </Link></button>
--- src/Components/Project/Projectdata.js
+++ src/Components/Project/Projectdata.js
@@ -1,55 +1,70 @@
 export const Projectdata = [
-
     {
         id: 1,
+        name: "Weather App",
+        date: "6th March, 2023",
+        description: "Created an app using React.Js. A real time weather app which shows current weather of a particular city in the world, it also shows different weather properties like pressure, wind, humidity etc. You can see it working here:",
+        appLink: "https://react-weatherr-appp.netlify.app/",
+        sourceLink: "https://github.com/asadraiyan/react-weather-app"
+    },
+    {
+        id: 2,
+        name: "Stopwatch",
+        date: "26th February, 2023",
+        description: "Created an app using React.Js. A stopwatch is a watch with buttons which you press at the beginning and end of an event, so that you can measure exactly how long it takes. You can see it working here:",
+        appLink: "https://stopwatch-appp.netlify.app/",
+        sourceLink: "https://github.com/asadraiyan/react-stopwatch"
+    },
+    {
+        id: 3,
         name: "Food Gallery App",
-        date: "27th January, 2023",
+        date: "10th February, 2023",
         description: "Created an app using React.Js. It is a food gallery app which allows you to select different types of food categories. You can see it working here:",
         appLink: "https://food-gallery-app.netlify.app",
         sourceLink: "https://github.com/asadraiyan/React-food--filter-gallery"
     },
     {
-        id: 2,
+        id: 4,
         name: "Quiz App",
-        date: "31st January, 2023",
+        date: "5th February, 2023",
         description: "Created an app using React.Js. It is an application which asks different types of questions based on web development. After finishing quiz you can view your score. You can see it working here:",
         appLink: "https://quizzz-app-react.netlify.app",
         sourceLink: "https://github.com/asadraiyan/react-quiz-app"
     },
     {
-        id: 3,
+        id: 5,
         name: "Todo List App",
-        date: "25th January, 2023",
+        date: "30th January, 2023",
         description: "Created an app using React.Js . It is an application that allows to manage a list of tasks to do. It performs adding, updating, deleting and toggling state of each task. You can see it working here:",
         appLink: "https://react-todo-app-asad.netlify.app",
         sourceLink: "https://github.com/asadraiyan/react-todos-list-app"
     },
     {
-        id: 4,
+        id: 6,
         name: "TextUtils App",
-        date: "22nd January, 2023",
+        date: "24th January, 2023",
         description: "Created an app using React.Js. It has multiple functionality for text editing according to user. It has both light and dark mode, it also shows the summarry of your text as a preview and also shows the number of words and characters. You can see it working here:",
         appLink: "https://react-textutils-appp.netlify.app/",
         sourceLink: "https://github.com/asadraiyan/react-textutils"
     },
     {
-        id: 5,
+        id: 7,
         name: "Tic Tac Toe Game",
-        date: "19th January, 2021",
+        date: "20th January, 2023",
         description: "Created an app using HTML CSS & Javascript. Tic-tac-toe is a simple, two-player game that, if played optimally by both players, will always result in a tie. The game is also called noughts and crosses or Xs and Os. A player wins a game if three marks of the same kind is connected in a line. You can see it working here:",
         appLink: "https://my-tic-tac-toe-appp.netlify.app/",
         sourceLink: "https://github.com/asadraiyan/Tic-Tac-Toe-Game"
     },
     {
-        id: 6,
+        id: 8,
         name: "Calendar App",
-        date: "14th January, 2023",
+        date: "16th January, 2023",
         description: "Created an app using HTML CSS & Javascript.The JavaScript Calendar is a control to display the date and days of the week. It provides month, year, and decade view options to quickly navigate to the desired date. It also shows the current date whenever you open it. You can see it working here:",
         appLink: "https://dynamic-calendar-app.netlify.app/",
         sourceLink: "https://github.com/asadraiyan/Calendar-app"
     },
     {
-        id: 7,
+        id: 9,
         name: "Weather App",
         date: "10th January, 2023",
         description: "Created an app using HTML CSS & Javascript. A real time weather app which shows current weather of a particular city in the world. You can see it working here:",
@@ -58,28 +73,29 @@ export const Projectdata = [
     },
 
     {
-        id: 8,
+        id: 10,
         name: "Calculator App",
-        date: "2nd January, 2023",
+        date: "3rd January, 2023",
         description: "Created an app using HTML CSS & Javascript. In this Calculator app, we can perform basic operations like addition, multiplication, subtraction, and division. You can see it working here.:",
         appLink: "https://asad-calculator.netlify.app",
         sourceLink: "https://github.com/asadraiyan/Calculator-app"
     },
     {
-        id: 9,
+        id: 11,
         name: "Analog Clock",
-        date: "26th December, 2022",
+        date: "30th December, 2022",
         description: "Created an app using HTML CSS and JavaSript. A simple analog clock which represent the current time. You can see it working here:",
         appLink: "https://analog-jsclock.netlify.app",
         sourceLink: "https://github.com/asadraiyan/Analog-Clock-app"
     },
     {
-        id: 10,
+        id: 12,
         name: "Food Delivery App",
-        date: "17 December, 2022",
+        date: "25 December, 2022",
         description: "Created an app using HTML and CSS. It represents the various food services like food ordering ,catering, etc. You can see it working here:",
         appLink: "https://online-meal-delivery.netlify.app",
         sourceLink: "https://github.com/asadraiyan/Food-delivery-app"
     },
+
 
 ]
