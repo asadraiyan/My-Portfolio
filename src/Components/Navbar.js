ad8e22705efa2b70fc4e72c87288e2aa0839e2ba
asadraiyan <asadraiyan001@gmail.com>
2023-05-27 21:25:11 +0530

    added active classes on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,6 +6,7 @@ import { useMediaQuery } from "react-responsive";
 const Navbar = () => {
     const isMobile = useMediaQuery({ query: "(max-width: 1000px)" });
     const [showMediaIcons, setshowMediaIcons] = useState(true)
+    const [activeClass, setActiveClass] = useState("Home")
 
     const handleClick = () => {
         setshowMediaIcons(!showMediaIcons)
@@ -18,10 +19,10 @@ const Navbar = () => {
                     <span>Asad Raiyan</span>
                 </div>
                 <div className={isMobile ? (showMediaIcons ? "sideNav hidden" : "sideNav") : "navlink"}>
-                    <Link to="/" onClick={handleClick}>Home</Link>
-                    <Link to="/About" onClick={handleClick}>About</Link>
-                    <Link to="/Projects" onClick={handleClick}>Projects</Link>
-                    <Link to="/Contact" onClick={handleClick}>Contact</Link>
+                    <div className={activeClass === "Home" ? "active" : ""} onClick={() => setActiveClass("Home")}><Link to="/" onClick={handleClick}>Home</Link> </div>
+                    <div className={activeClass === "About" ? "active" : ""} onClick={() => setActiveClass("About")}><Link to="/About" onClick={handleClick}>About</Link></div>
+                    <div className={activeClass === "Projects" ? "active" : ""} onClick={() => setActiveClass("Projects")}><Link to="/Projects" onClick={handleClick}>Projects</Link></div>
+                    <div className={activeClass === "Contact" ? "active" : ""} onClick={() => setActiveClass("Contact")}>  <Link to="/Contact" onClick={handleClick}>Contact</Link></div>
                 </div>
                 {isMobile ? (
                     <div className="menu-icons" onClick={handleClick}>
