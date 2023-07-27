de8844988e35928e95b2536adca708fe07bd8855
Asad Raiyan <asadraiyan001@gmail.com>
2023-07-27 15:05:40 +0530

    few changes in css

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import HomeImage from './components/HomeImage'
-import HomePageProject from './components/HomePageProject'
-import "./Home.css"
+import React from "react";
+import HomeImage from "./components/HomeImage";
+import HomePageProject from "./components/HomePageProject";
+import "./Home.css";
+import Skill from "../About/components/Skill";
 
 const Home = () => {
   return (
     <div>
       <HomeImage />
       <HomePageProject />
+      <Skill />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
